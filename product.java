4c367a21d1f035a10d362545160b6dbce9739934
nisKULDEEP <kuldeepnishad.singh@gmail.com>
2021-11-22 16:44:14 +0530

    product.java

---
+++ product.java
@@ -0,0 +1,13 @@
+public class product {
+
+        public static void main(String [] args) {
+            int a = 33;
+            int b = 3434;
+           
+            System.out.println(a*b);
+        }
+        
+    
+    
+    
+}
